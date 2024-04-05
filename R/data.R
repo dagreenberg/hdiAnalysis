976f3b17eda171af1cb6da76932167f4377bf82c
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-04-05 15:49:06 -0700

    Add mcmc values, document().

---
+++ R/data.R
@@ -0,0 +1,10 @@
+##' Help files for data objects that are not committed and so not on GitHub.
+##'
+##' @format A tibble with each row representing an MCMC sample, and columns:
+##' \describe{
+##'   \item{Virgin:}{presumably gives `R0`, TODO double check}
+##'   \item{`1966`:`2027`:}{year for each recruitment}
+##'  }
+##' @author Andrew Edwards
+##' @source Generated from running `data-raw/hake.R`
+"hake_recruitment_mcmc"
