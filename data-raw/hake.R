397277723bd55f465223cd25b0ee4931def08dc3
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-05-13 16:57:37 -0700

    Add long 32000 chain in there (not pushing the .rda though).

--- data-raw/hake.R
+++ data-raw/hake.R
@@ -64,3 +64,54 @@ usethis::use_data(hake_recruitment_mcmc,
 #                                 class(hake_recruitment_new))
 # attr(hake_recruitment_new, "axis_name") <-
 #  "Pacific Hake recruitment (billions of age-0 fish)"
+
+
+
+# Trying to get experimental MCMC chains from server, as being used for Kelli's
+# manuscript. Use exp for suffix for now.
+
+hake_dir_exp <- paste0(here::here(),
+                       "/data-raw/hake-",
+                       assess_yr,
+                       "/02-version-05-test-models/")
+
+hake_14_long <- readRDS(paste0(hake_dir_exp,
+                            "14-long-base.rds"))
+# Contains all MCMC results and maybe more, as a list object.
+# There's an hake.Rdata file in mcmc/ on the server but I don't think
+#    that's useful, has things like number of chains etc, don't think it has results.
+
+# Using some from hake-assessment-2024::mcmc_save()
+dat <- as_tibble(hake_14_long$mcmc)
+
+keep <- select(dat,
+               "Iter":"SR_BH_steep",
+               "SSB_Virgin":"Recr_2027",
+               "Bratio_1967":"Recr_unfished",
+               "B_MSY/SSB_unfished":"ForeCatch_2027",
+               "Dyn_Bzero_Virg":"ln(SSB)_2023")
+
+names(keep) = gsub(pattern = "SSB_",
+                   replacement = "B_",
+                   x = names(keep))
+
+names(keep) = gsub(pattern = "Recr_",
+                   replacement = "R_",
+                   x = names(keep))
+
+keep
+
+# Copying from above to just have recruitment, need 14_long
+hake_recruitment_mcmc_14_long <- dplyr::select(keep,
+                                               "R_Virgin",
+                                               "R_1966":"R_2027")
+
+names(hake_recruitment_mcmc_14_long) <- gsub(pattern = "R_",
+                                             replacement = "",
+                                             x = names(hake_recruitment_mcmc_14_long))
+
+hake_recruitment_mcmc_14_long <- as_tibble(hake_recruitment_mcmc_14_long/1e6)    # Convert from thousands
+                                                                 # to billions of fish
+
+usethis::use_data(hake_recruitment_mcmc_14_long,
+                  overwrite = TRUE)
