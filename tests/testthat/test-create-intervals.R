202ae83c75d86c7230741a1baa96aad3bae6c623
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-06-06 09:07:24 -0700

    Add quick test, should stop GHA fails.

--- tests/testthat/test-create-intervals.R
+++ tests/testthat/test-create-intervals.R
@@ -1,11 +1,11 @@
-# calc_density(). Quick test just to keep GHA happy for now.
-test_that("cacl_density results on hake 2021 recruitment do not change; only checks one value for now to get working", {
-  one_year_mcmc <- dplyr::pull(hake_recruitment_mcmc, `2021`)
-  res_calc_density <- calc_density(one_year_mcmc)
+# create_intervals(). Quick test just to keep GHA happy for now.
+test_that("create_intervals results on hake 2021 recruitment do not change; only checks one value for now to get working", {
+  res_2021_test <- create_intervals(rec_2021)
 
-  vec_results <- c(10.1873, 4.085088, 29.49938,  2.702156,  25.55708, 25.4143, 22.85492,
-    2.559376, 21 ,0.03289304, 123, 0.003572334, 1, 16, 0.008976952, 108,
-    0.006995915) # calculated 2024-05-16
+  #vec_results <- c(10.1873, 4.085088, 29.49938,  2.702156,  25.55708, 25.4143, 22.85492,
+  #  2.559376, 21 ,0.03289304, 123, 0.003572334, 1, 16, 0.008976952, 108,
+  #  0.006995915) # calculated 2024-05-16
+  vec_results <- c(10.187300,  4.085088, 29.499385,  2.529758, 25.517941) # calculated 2024-06-05
   # ints_here <- tibble::tibble_row(t(vec_results))
   ## names(ints_here) <- c("median",
   ##                       "2.5",
@@ -23,7 +23,7 @@ test_that("cacl_density results on hake 2021 recruitment do not change; only che
   ##                       "y_low_hdi_interp",
   ##                       "i_high_hdi",
   ##                       "y_high_hdi_interp")
-  expect_equal(res_calc_density$intervals[1:5] %>% as.numeric(),
-               vec_results[1:5],
+  expect_equal(res_2021_test$intervals[1:5] %>% as.numeric(),
+               vec_results,
                tolerance = 1e-5)
 })
