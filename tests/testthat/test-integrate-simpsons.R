3091b73c1e94f67600912bbd486147681f879351
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-07-25 10:28:34 -0700

    Simulation vignette for Simpson's rule code.

--- tests/testthat/test-integrate-simpsons.R
+++ tests/testthat/test-integrate-simpsons.R
@@ -15,7 +15,7 @@ test_that("integrate_simpsons() gives sensible results", {
                                       n = n_for_density))
 
   expect_equal(res_2, 1, tolerance = 1e-04)   # worked locally with 1e-05 but
-                                        # not on GHA
+                                              # not on GHA
 
   # Lognormal distribution parameterised based on saved simulation
   res_3 <- integrate_simpsons(density(rlnorm(num_samples,
@@ -24,6 +24,34 @@ test_that("integrate_simpsons() gives sensible results", {
                                       n = n_for_density))
 
   expect_equal(res_3, 1, tolerance = 1e-05)
+
+  # Now try domain option
+
+  # Normal distribution
+  set.seed(42)
+  dens_4 <- density(rnorm(num_samples),
+                    n = n_for_density)
+  res_4_left <- integrate_simpsons(dens_4,
+                                   domain = c(min(dens_4$x), 0))
+  res_4_right <- integrate_simpsons(dens_4,
+                                   domain = c(0, max(dens_4$x)))
+
+  expect_equal(res_4_left, 0.5, tolerance = 1e-02)    # Needed small tolerance,
+                                        # could be since doing around 0 (the
+                                        # mode) it's easy for random samples to
+                                        # switch between left and right; quite a
+                                        # strict condition
+                                        # but they sum to 1 (should, as same as res_4)
+  expect_equal(res_4_right, 0.5, tolerance = 1e-02)
+  expect_equal(res_4_left + res_4_right, 1, tolerance = 1e-05)
+
+  # Maybe hoping for too much accuracy; need a simulation experiment on multiple
+  # sets of random samples.
+  # expect_equal(integrate_simpsons(dens_4,
+  #                                 domain = c(-2, -1)),
+  #              pnorm(-1) - pnorm(-2))
+
+
 
   # Expected errors
   expect_error(integrate_simpsons(77))
--- tests/testthat/test-intermediate-y.R
+++ tests/testthat/test-intermediate-y.R
@@ -6,9 +6,11 @@ test_that("intermediate_y() gives correct results", {
   expect_equal(intermediate_y(9, 4, 10, 100, 120),
                116.66666666)
 
+  expect_equal(intermediate_y(14, 10, 20, 200, 100),
+               160)
+
+
   expect_error(intermediate_y(8, 10, 7, 100, 120))
   expect_error(intermediate_y(5, 6, 7, 100, 120))
   expect_error(intermediate_y(8, 6, 7, 100, 120))
-
-  # TODO check works for decreasing limb, assuming densities aren't negative
 })
