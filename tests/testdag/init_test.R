6db0ac88bf2a15ca295ff30d60cb9bf1221a43a2
Dan Greenberg <dan.greenberg01@gmail.com>
2024-08-27 15:06:29 -0700

    Update init_test.R

--- tests/testdag/init_test.R
+++ tests/testdag/init_test.R
@@ -10,9 +10,11 @@ smax=data.frame(l.hdi.95=NA,u.hdi.95=NA,l.eti.95=NA,u.eti.95=NA)
 umsy=data.frame(l.hdi.95=NA,u.hdi.95=NA,l.eti.95=NA,u.eti.95=NA)
 for(l in 1:50){
   par_l[[l]]=read.csv(here('pars',list.files(here('pars'))[l]))
-  smsy[l,]=create_intervals(par_l[[l]]$Smsy,allow_hdi_zero=T)$intervals[2:5]
-  smax[l,]=create_intervals(par_l[[l]]$Smax,allow_hdi_zero=T)$intervals[2:5]
-  umsy[l,]=create_intervals(par_l[[l]]$Umsy,allow_hdi_zero=T)$intervals[2:5]
+  smsy[l,]=create_intervals(par_l[[l]]$Smsy,from=NULL)$intervals[2:5]
+  l=16
+  smax[l,]=create_intervals(par_l[[l]]$Smax,from=NULL)$intervals[2:5]
+  
+  umsy[l,]=create_intervals(par_l[[l]]$Umsy[par_l[[l]]$Umsy>0],from=NULL)$intervals[2:5]
   
   }
 
